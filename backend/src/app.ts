01a5b8c30802191c538cdd61d1220cb78256ce80
Zavista <84294353+Zavista@users.noreply.github.com>
2024-09-08 19:35:15 -0400

    added error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import NoteModel from "./models/note";
 
-const app = express(); // an instance of an express application
+const app = express();
 
-// when a GETrequest is sent to the root url, the call back function is executed with req being the data in the request and res being what is sent back
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found."));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) {
+    // Type guard: error is an instance of Error
+    errorMessage = error.message;
+  }
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
