94e2d3c3073ebad55fe3abf113a94c25feb0110e
Zavista <84294353+Zavista@users.noreply.github.com>
2024-09-25 19:33:54 -0400

    added login logic

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,6 +3,7 @@ import * as UserController from "../controllers/user";
 
 const router = express.Router();
 
+router.get("/", UserController.getAuthenticatedUser);
 router.post("/signup", UserController.signUp);
-
+router.post("/login", UserController.login);
 export default router;
